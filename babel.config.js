ff2a57179af38ce8be711630ccdcddc5a9d6b45f
welinton <welinton.h.r@hotmail.com>
2024-06-26 10:16:49 +0100

    Add module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: { "^~(.+)": "./src/\\1" },
+          extensions: [
+            ".js",
+            ".ts",
+            ".jsx",
+            ".tsx",
+            ".json",
+            ".ios.js",
+            ".native.js",
+            ".android.js",
+          ],
+        },
+      ],
+    ],
   };
 };
