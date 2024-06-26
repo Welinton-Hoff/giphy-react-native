1f101474643e1d4b3eda67ec70aee125eebaea46
welinton <welinton.h.r@hotmail.com>
2024-06-26 11:21:15 +0100

    Install react-query

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import "react-native-gesture-handler";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import theme from "./theme";
 import { Routes } from "./routes";
 
+const queryClient = new QueryClient();
+
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
